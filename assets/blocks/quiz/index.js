cee3860efd6d2e4bf1d8c9057538773c3789fe38
Jake Oehler Morrison <jake.morrison@automattic.com>
2021-02-26 22:07:02 +0000

    Add the quiz block in the frontend (WIP)

--- assets/blocks/quiz/index.js
+++ assets/blocks/quiz/index.js
@@ -1,10 +1,40 @@
 /**
+ * WordPress dependencies
+ */
+import { select, subscribe, dispatch } from '@wordpress/data';
+import { createBlock } from '@wordpress/blocks';
+
+/**
  * Internal dependencies
  */
 import registerSenseiBlocks from '../register-sensei-blocks';
+import { getFirstBlockByName } from '../../shared/helpers/blocks';
 
 import questionBlock from './question-block';
 import quizBlock from './quiz-block';
 import './quiz-store';
 
 registerSenseiBlocks( [ quizBlock, questionBlock ] );
+
+let hasQuestions;
+const unsubscribe = subscribe( () => {
+	hasQuestions = select( 'core/editor' ).getEditedPostAttribute( 'meta' )
+		?._quiz_has_questions; // eslint-disable-line camelcase
+
+	const blocks = select( 'core/block-editor' ).getBlocks();
+	if ( hasQuestions === undefined || blocks.length === 0 ) {
+		return;
+	}
+
+	const { insertBlock } = dispatch( 'core/block-editor' );
+	const quizBlockInstance = getFirstBlockByName( 'sensei-lms/quiz', blocks );
+
+	if ( ! quizBlockInstance && hasQuestions ) {
+		setTimeout(
+			() => insertBlock( createBlock( 'sensei-lms/quiz', {} ) ),
+			1
+		);
+	}
+
+	unsubscribe();
+} );
---
+++ includes/rest-api/class-sensei-rest-api-lessons-controller.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Sensei REST API: Sensei_REST_API_Lessons_Controller class.
+ *
+ * @package sensei-lms
+ * @since 3.9.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly.
+
+/**
+ * A REST controller for Sensei LMS lessons CPT.
+ *
+ * @since 3.9.0
+ *
+ * @see WP_REST_Posts_Controller
+ */
+class Sensei_REST_API_Lessons_Controller extends WP_REST_Posts_Controller {
+	/**
+	 * Constructor.
+	 *
+	 * @param string $post_type Post type.
+	 */
+	public function __construct( $post_type ) {
+		parent::__construct( $post_type );
+
+		register_post_meta(
+			'lesson',
+			'_quiz_has_questions',
+			[
+				'show_in_rest'  => true,
+				'single'        => true,
+				'type'          => 'boolean',
+				'auth_callback' => function( $allowed, $meta_key, $post_id ) {
+					return current_user_can( 'edit_post', $post_id );
+				},
+			]
+		);
+	}
+}
